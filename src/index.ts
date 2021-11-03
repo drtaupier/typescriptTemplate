d57888d879c07820ccf34c3a26c1e0c06b5de2cc
David Rivera <drtaupier@gmail.com>
2021-11-02 21:47:35 -0400

    I configured the routes inside to routes file

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,10 @@
 import express from 'express';
+import routes from './routes/index';
+
 const app = express();
-
-
 const port = 3000;
 
-app.get('/', (req,res)=>{
-    res.send('Hello world!');
-});
+app.use('/api', routes);
 
 app.listen(port, ()=>{
     console.log(`Server started at localhost:${port}`);
