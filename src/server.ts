9d3f392b0db328405c7420583cb619c9c7db4c78
David Rivera <drtaupier@gmail.com>
2021-11-02 23:05:14 -0400

    Configure mediante csvtojson la lectura del archivo users.csv, convertirlo a json e indicarle que si un campo esta vacio lo complete con Missing data

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,35 @@
 import express from 'express';
 import routes from './routes/index';
-import students from './routes/api/students';
-import teachers from './routes/api/teachers';
+import { promises as fsPromises} from 'fs';
+import csv from 'csvtojson';
 
 const app = express();
 const port = 3000;
 
 app.use('/api', routes);
+
+const inputFile = './users.csv';
+const outputFile = 'users.json';
+
+app.get('/convert', (req, res)=>{
+    res.send('Converting in progress!');
+    csv()
+    .fromFile(inputFile)
+    .then((data)=>{
+        let newData = data.map( (item: {
+            first_name: string; last_name: string;
+            phone: string; }) =>{
+                let first = item.first_name;
+                let last = item.last_name;
+                let phone = item.phone;
+                if(item.phone === ""){
+                    phone = "Missing data";
+                }
+                return {first, last, phone};
+            });
+            fsPromises.writeFile(outputFile, JSON.stringify(newData));
+    });
+});
 
 app.listen(port, ()=>{
     console.log(`Server started at localhost:${port}`);
